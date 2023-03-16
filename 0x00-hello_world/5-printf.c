68e7d41ad2213184411cb2e6eb8399ccb62c7829
selvia <selviakhalil4@gmail.com>
2023-03-16 15:31:59 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is first function of every c program
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
