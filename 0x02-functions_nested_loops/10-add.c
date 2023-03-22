08c4842b41680d4574567854fa9c1938df7370c2
selvia <selviakhalil4@gmail.com>
2023-03-22 13:37:36 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function add number
+ * @a: int added to b
+ * @b: int addede to a
+ * Return: return sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
