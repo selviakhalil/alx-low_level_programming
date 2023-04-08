801682d11fc108159ca9444d6ffc1d10f03ad41f
selvia <selviakhalil4@gmail.com>
2023-04-08 12:27:54 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - this is main function of every c
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: array of arguments
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
